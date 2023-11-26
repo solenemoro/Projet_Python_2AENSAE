966138c39e71cd6c8babcb6f27bdd88866d1d8d2
mnoslier <matthieu.noslier@ensae.fr>
2023-11-26 22:20:21 +0000

    Sunday evening
    Just the beginning

---
+++ try.py
@@ -0,0 +1,13 @@
+import pandas
+
+"""
+# Type in the terminal
+pip install openpyxl
+"""
+
+# Lire le fichier excel
+pandas.read_excel("https://www.insee.fr/fr/statistiques/fichier/3568650/BPE_gammes_2021_internet_v2.xlsx")
+
+pandas.read_csv("https://www.insee.fr/fr/statistiques/fichier/3568638/bpe21_ensemble_xy_csv.zip")
+
+# Zipfile
