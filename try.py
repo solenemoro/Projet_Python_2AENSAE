a49b6a23ecca8b306197a182a4ddbe996db415f2
mnoslier <matthieu.noslier@ensae.fr>
2023-11-27 12:20:17 +0000

    Adding Zip

--- try.py
+++ try.py
@@ -1,4 +1,7 @@
 import pandas
+import requests
+import io
+import zipfile
 
 """
 # Type in the terminal
@@ -8,6 +11,26 @@ pip install openpyxl
 # Lire le fichier excel
 pandas.read_excel("https://www.insee.fr/fr/statistiques/fichier/3568650/BPE_gammes_2021_internet_v2.xlsx")
 
-pandas.read_csv("https://www.insee.fr/fr/statistiques/fichier/3568638/bpe21_ensemble_xy_csv.zip")
-
 # Zipfile
+
+# ZipInfo.filename("https://www.insee.fr/fr/statistiques/fichier/3568638/bpe21_ensemble_xy_csv.zip")
+"""
+doc = zipfile.ZipFile('https://www.insee.fr/fr/statistiques/fichier/3568638/bpe21_ensemble_xy_csv.zip', 'r')
+
+zipfile.ZipFile.open('doc')
+"""
+
+# zipfile.extract
+
+url = 'https://www.insee.fr/fr/statistiques/fichier/3568638/bpe21_ensemble_xy_csv.zip'
+
+r = requests.get(url)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+
+
+"""
+# cf https://gist.github.com/ZeccaLehn/140edc75ff9d2c7cf9f660028763c9f5
+
+"""
+
